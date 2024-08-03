0787147b3fb42aeb1a5e0f4d123fdde3337f6f55
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-08-03 22:04:20 +0200

    Added routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'your-profile',
+    component: ProfileComponent,
+  },
+  {
+    path: 'sign-up',
+    component: RegisterComponent,
+  },
+  {
+    path: 'sign-in',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
