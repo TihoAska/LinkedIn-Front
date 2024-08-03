1d9e695a0e5c88a7b04fcb28fea4299ab884e6b0
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-08-03 22:05:13 +0200

    Check if user logged in.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LinkedIn';
+
+  constructor(
+    private router : Router,
+    private userService : UserService,
+  ) {
+    
+    
+  }
+
+  ngOnInit(){
+    if(typeof window !== 'undefined'){
+      if(localStorage && localStorage.getItem('accessToken')){
+        var user = this.userService.decodeUserFromToken(localStorage.getItem('accessToken'));
+        this.userService.$loggedUser.next(user);
+        this.router.navigate(['your-profile'])
+      } else{
+        this.router.navigate(['sign-up']);
+      }
+    }
+  }
 }
