f3b33c26ef0cecbb827e79c4f694bc5c10d78be6
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-08-30 19:00:32 +0200

    Get user when application starts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from './services/user.service';
+import { PageService } from './services/page.service';
+import { HelperService } from './services/helper.service';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +15,8 @@ export class AppComponent {
   constructor(
     private router : Router,
     private userService : UserService,
+    private pageService : PageService,
+    public helperService : HelperService
   ) {
     
     
@@ -22,8 +26,14 @@ export class AppComponent {
     if(typeof window !== 'undefined'){
       if(localStorage && localStorage.getItem('accessToken')){
         var user = this.userService.decodeUserFromToken(localStorage.getItem('accessToken'));
-        this.userService.$loggedUser.next(user);
-        this.router.navigate(['your-profile'])
+        this.userService.getUserByIdWithUserDetails(user.id).subscribe(res => {
+          console.log(res?.education[0]?.startTime);
+          console.log(res?.education[0]?.endTime);
+
+          this.userService.$loggedUser.next(res);
+
+          this.router.navigate(['your-profile', 'profile-details']);
+        });
       } else{
         this.router.navigate(['sign-up']);
       }
