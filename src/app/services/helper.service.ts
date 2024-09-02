63a94a1879c9c1d768eb9fe163754c6923b7a074
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-09-02 20:00:23 +0200

    Added showEditEducationWindow observable and formatted other observables.

--- src/app/services/helper.service.ts
+++ src/app/services/helper.service.ts
@@ -5,11 +5,12 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class HelperService {
-  $dimBackground : BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  $showAddExperienceWindow : BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  $showEditExperienceWindow : BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  $showProfile : BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
-  $editingExperienceId : BehaviorSubject<number> = new BehaviorSubject<number>(-1);
+  $dimBackground: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  $showAddExperienceWindow: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  $showEditExperienceWindow: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  $showProfile: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  $editingExperienceId: BehaviorSubject<number> = new BehaviorSubject<number>(-1);
+  $showEditEducationWindow: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   constructor() { }
 }
